319b2dc22c7f5fc06da1a1f72d701e3cd212521c
Somu Dey <109870389+somu-dey@users.noreply.github.com>
2024-05-30 16:21:18 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -10,8 +10,13 @@ import Landing from "./components/Landing";
 // import { userAuthContextProvider } from "./context/UserAuthContext";
 import MainAds from "./components/MainAds";
 import About from "./HeaderNav/About";
+import Admin from "./admin/Admin";
+import AdminPanel from "./admin/AdminPanel";
 // import ProductDesc from "./components/ProductDesc";
 import AdCar from "./PostAd/AdCar";
+import AdBike from "./PostAd/AdBike";
+import MyWishlist from "./HeaderNav/MyWishlist";
+import ChatUsers from "./chats/ChatUsers";
 import AdMobile from "./PostAd/AdMobile";
 import AdElectronics from "./PostAd/AdElectronics";
 import MyAds from "./components/MyAds";
@@ -20,7 +25,9 @@ import { AnimatePresence } from "framer-motion";
 import { NextUIProvider } from "@nextui-org/react";
 import ForgotPassword from "./components/ForgotPass";
 import Contact from "./HeaderNav/Contact";
-import Chat from "./components/Chat";
+import Chat from "../src/chats/Chat";
+import MyProfile from "./components/MyProfile";
+import UserChats from "./admin/UserChats";
 function App() {
   const location = useLocation();
   return (
@@ -32,15 +39,23 @@ function App() {
           <Route exact path="/" element={<Landing />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Signup />} />
-          <Route path="/adproduct" element={<MainAds />} />
+          <Route path="/mainads" element={<MainAds />} />
           <Route path="/aboutus" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/adcar" element={<AdCar />} />
           <Route path="/admobile" element={<AdMobile />} />
           <Route path="/adelectronic" element={<AdElectronics />} />
+          <Route path="/adbike" element={<AdBike />} />
           <Route path="/myads" element={<MyAds />} />
+          <Route path="/mywishlist" element={<MyWishlist />} />
           <Route path="/forgotpassword" element={<ForgotPassword />} />
           <Route path="/chat" element={<Chat />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/adminpanel" element={<AdminPanel />} />
+          <Route path="/chatUsers" element={<ChatUsers />} />
+          <Route path="/profile" element={<MyProfile />} />
+          <Route path="/userChats" element={<UserChats />} />
+
           <Route path="/productdetails/:id" element={<ProductDetails />} />
         </Routes>
       </AnimatePresence>
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
